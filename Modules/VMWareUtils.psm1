7dbba220b74bf5ec8e556af324bea060b00d557f
Samir Seth <samirkseth@gmail.com>
2011-06-29 00:12:21 +0100

    Added VMWareUtils

---
+++ Modules/VMWareUtils.psm1
@@ -0,0 +1,40 @@
+$script:BASEENVLOCATION="Template Experiment"
+
+$script:BASEENVLOCATION="Template Experiment"
+$script:VMHOST="sifyvmw05.thoughtworks.com"
+$script:BASETEMPLATE="W2003-DotNetTemplate"
+$script:DATASTORE="tliscsi18"
+$script:OSCUSTOMIZATIONSPEC="Windows 2003"
+
+
+Function Create-Machine([String]$MachineName, [String] $Location)
+{
+	Write-Host "Creating Machine - $MachineName"
+
+	New-Vm  -vmhost $VMHOST	`
+	        -Name $MachineName `
+			-Location $Location `
+			-Template $BASETEMPLATE	`
+			-Datastore $DATASTORE	`
+			-OSCustomizationSpec $OSCUSTOMIZATIONSPEC
+
+}
+
+Function Create-Environment($envconfstr)
+{
+	[xml] $env = $envconfstr
+	
+	New-Folder -Name $env.Environment.name -Location $script:BASEENVLOCATION 
+	
+	foreach ($machinetypeentry in $env.Environment.Machines.Machine)
+	{
+		$machinetype = $machinetypeentry.Type
+		$qty = $machinetypeentry.Quantity
+			
+		for ($i = 1; $i -le $qty; $i++) 
+		{
+			$machinename = "{0}{1:0#}.{2}" -f $machinetypeentry.BaseName, $i, $env.Environment.name
+			Create-Machine $machinename $env.Environment.name
+		}
+	}
+}
